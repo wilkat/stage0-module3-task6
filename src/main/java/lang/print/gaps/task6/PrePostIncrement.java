64ab332323f2f9103536c16a40ba52c0a1a7c569
wilkat <katarzyna.f.wilk@gmail.com>
2022-11-01 19:26:45 +0100

    new

--- src/main/java/lang/print/gaps/task6/PrePostIncrement.java
+++ src/main/java/lang/print/gaps/task6/PrePostIncrement.java
@@ -8,9 +8,12 @@ public class PrePostIncrement {
         int d = 15;
 
         // use post-increment operator in the mathematical expression  
-        int x =   a   + b +   c   + d;
+        a++;
+        int x =   a  + b +   c   + d;
+
         System.out.println("Expected x is 40, x = " + x);
-        // print the updated value of a, b, c, and d  
+        // print the updated value of a, b, c, and d
+        c++;
         System.out.println("The updated value of a = " + a + ", b = " + b + ", c = " + c + " and d = " + d);
 
     }
